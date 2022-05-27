f7303472a8f44681b3583c367f689ad75e31b966
Xsufu <aluno.macarrao@gmail.com>
2022-05-28 04:37:01 +0700

    Добавлена кнопка "назад"

--- app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -36,6 +36,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // TODO: Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
     }
 }
